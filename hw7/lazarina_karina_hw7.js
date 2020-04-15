bee8357c2efa62caabc9bcb204639e8a5b4d720d
Karina Lazarina <karinyao27@gmail.com>
2020-04-15 18:58:51 +0300

    home work 7(js)

--- hw7/lazarina_karina_hw7.js
+++ hw7/lazarina_karina_hw7.js
@@ -67,7 +67,6 @@
 //         div.style.display = 'none';
 //     }
 // }
-// //как можно сделать чтоб работало больше 1 раза?
 // menu.appendChild(div);
 // document.body.appendChild(menu);
 
@@ -306,12 +305,12 @@
 //                 {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
 //                 {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
 //             ];
-// Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива.
-// Фильтры могут работать как вместе так и по отдельности.
-// 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
-// 2й - оставляет старше 29 лет включительно
-// 3й - оставляет тех у кого город киев
-// Данные выводить в документ
+// // Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива.
+// // Фильтры могут работать как вместе так и по отдельности.
+// // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
+// // 2й - оставляет старше 29 лет включительно
+// // 3й - оставляет тех у кого город киев
+// // Данные выводить в документ
 // let result = document.getElementById('checked');
 // function allUsers(users) {
 //     for (const user of users) {
@@ -382,12 +381,82 @@
 //task14
 // //*** При виділені сегменту тексту на сторінці він стає жирний/курсивний/або якось іншим способом змінює свій стан
 //
-// window.onselect = () => {
+// window.onmouseup = () => {
 //     let selection = document.getSelection();
-//     console.log(selection.rangeCount.valueOf());
-//     console.log(selection);
+//     let range = selection.getRangeAt(0);
+//     let tempEl = document.createElement("i");
+//     range.surroundContents(tempEl);
+//
 // }
 
+
+//task13.2.0
+////-- взять массив пользователей
+let usersWithAddress = [
+                {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+                {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+                {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+                {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+                {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+                {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+                {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+                {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+                {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+                {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+                {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+            ];
+// // Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива.
+// // Фильтры могут работать как вместе так и по отдельности.
+// // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
+// // 2й - оставляет старше 29 лет включительно
+// // 3й - оставляет тех у кого город киев
+// // Данные выводить в документ
+
+
+let result = document.getElementById('checked');
+let form = document.querySelector("form");
+let selectorAll = document.querySelectorAll("input");
+
+function allUsers(users) {
+    for (const user of users) {
+        let div = document.createElement('div');
+        div.innerHTML = `${JSON.stringify(user)} <hr>`;
+        result.appendChild(div);
+    }
+}
+allUsers(usersWithAddress);
+
+form.onclick = () => {
+    let arr = usersWithAddress.filter(()=>true);
+    result.innerText = '';
+    for (let i=0; i<selectorAll.length; i++) {
+        if (selectorAll[i].checked){
+            if (i === 0){
+                arr = arr.filter(value => {
+                    if (value.status){
+                        return true;
+                    }
+                })
+            }
+            if (i === 1){
+                arr = arr.filter(value => {
+                    if (value.age >= 29){
+                        return true;
+                    }
+                })
+            }
+            if (i === 2){
+                arr = arr.filter(value => {
+                    if (value.address.city === 'Kyiv'){
+                        return true;
+                    }
+                })
+            }
+
+        }
+    }
+    allUsers(arr);
+}
 
 
 
